c0995b2a09d8912b19fe460eea4c67eb4fd20f78
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-07-25 19:57:34 +0200

    gateFAQ Function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,19 +6,35 @@
         <div class="padding1pc banner flex spaceEven flexEnd">
             <span>richtigGutBewerben</span>
 
-                <span class="cursor fontSize50pc">Kontakt</span>
-                <span class="cursor fontSize50pc">FAQ</span>
-                <span class="cursor fontSize50pc">Impressum</span>
+                <span onclick="gate('contact')" class="cursor  fontSize50pc">Kontakt</span>
+                <span onclick="gate('faq')" class="cursor fontSize50pc">FAQ</span>
+                <span onclick="gate('imprint')" class="cursor fontSize50pc">Impressum</span>
 
         </div>
         <div class="padding1pc wContent">
+            <div id="contact">
+                contactInfo
+            </div>
+            <div id="faq">
+                frage Antwort Kacka
+            </div>
+            <div id="imprint">
+                mein Impressum
+            </div>
             @if(Auth::User())
                 <livewire:list-view />
             @endif
         </div>
     </div>
 @endsection
-
+<script>
+    function gate(key) {
+        if(document.getElementById(key).style.display == "none")
+        document.getElementById(key).style.display = "block";
+        else
+        document.getElementById(key).style.display = "none";
+    }
+</script>
 
 
 
