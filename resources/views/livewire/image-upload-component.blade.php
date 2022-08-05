d070f822a8bf18ef61e36195d9d30891ffb0c6fb
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-08-05 17:31:26 +0200

    slug

--- resources/views/livewire/image-upload-component.blade.php
+++ resources/views/livewire/image-upload-component.blade.php
@@ -1,4 +1,7 @@
 <div>
+    <div>
+
+    </div>
     <div class="container mt-5 pt-5">
         <div class="row mt-4">
             <div class="col-md-12">
@@ -30,22 +33,68 @@
 
 
                                         {{-- ImagePreview --}}
+                                        @if($image)
 
-{{-- 
-                                        @if ($image)
-                                            <img src="{{ $image->temporaryUrl() }}" width="100" alt="" class="mt-2">
-                                        @endif --}}
-                                        @if ($image)
-                                        <div class="imgUp">
-                                            <div style="height: 239px; width: 432px;" class="mask">
-                                                <img src="{{ $image->temporaryUrl() }}"  alt="" class="mt-2">
-                                            </div>
-                                        </div>
+                                                <div  id="mydivheader" style="height: 239px; width: 432px;" class="mask">
+                                                    <img id="mydiv" class="fade mt-2" src="{{ $image->temporaryUrl() }}"  alt="">
+                                                </div>
+
                                         
+                                            <script>
+                                            //Make the DIV element draggagle:
+                                            dragElement(document.getElementById("mydiv"));
+                                        
+                                            function dragElement(elmnt) {
+                                                var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+                                                if (document.getElementById(elmnt.id + "header")) {
+                                                    /* if present, the header is where you move the DIV from:*/
+                                                    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+                                                } else {
+                                                    /* otherwise, move the DIV from anywhere inside the DIV:*/
+                                                    elmnt.onmousedown = dragMouseDown;
+                                                }
+                                        
+                                                function dragMouseDown(e) {
+                                                    e = e || window.event;
+                                                    e.preventDefault();
+                                                    // get the mouse cursor position at startup:
+                                                    pos3 = e.clientX;
+                                                    pos4 = e.clientY;
+                                                    document.onmouseup = closeDragElement;
+                                                    // call a function whenever the cursor moves:
+                                                    document.onmousemove = elementDrag;
+                                                }
+                                        
+                                                function elementDrag(e) {
+                                                    e = e || window.event;
+                                                    e.preventDefault();
+                                                    // calculate the new cursor position:
+                                                    pos1 = pos3 - e.clientX;
+                                                    pos2 = pos4 - e.clientY;
+                                                    pos3 = e.clientX;
+                                                    pos4 = e.clientY;
+                                                    // set the element's new position:
+                                                    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                                                    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+                                                }
+                                        
+                                                function closeDragElement() {
+                                                    /* stop moving when mouse button is released:*/
+                                                    document.onmouseup = null;
+                                                    document.onmousemove = null;
+                                                }
+                                                }
+                                            </script>
                                         @endif
                                     </div>
 
+                                    
+
+
                                     <a href="https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images">https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images</a>
+                                    
+                                    
+                                    
                                     <div class="form-group text-center">
                                         <button type="submit" class="btn btn-primary w-50 mt-2"><div wire:loading wire:target="uploadImage" wire:key="uploadImage"><i class="fa fa-spinner fa-spin"></i></div> Hochladen</button>
                                     </div>
